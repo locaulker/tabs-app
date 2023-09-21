447bdcaef4e6a7e5b58c0cb152c14cd31e2ad1b2
Lawrence Caulker <locaulker@gmail.com>
2023-09-21 01:03:13 -0400

    fetched data and implemented loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react"
+
+const url = "https://course-api.com/react-tabs-project"
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
-};
-export default App;
+  const [isLoading, setIsLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  // currentItem
+
+  const fetchedJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setJobs(newJobs)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchedJobs()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    )
+  }
+
+  return <h2>Tabs Starter</h2>
+}
+export default App
