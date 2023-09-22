e99cb610efbe50691b743de9edb27fea722e73d1
Lawrence Caulker <locaulker@gmail.com>
2023-09-22 01:06:23 -0400

    implemented uuid

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,5 +1,21 @@
+import { FaAngleDoubleRight } from "react-icons/fa"
+import { v4 as uuidv4 } from "uuid"
+
 const Duties = ({ duties }) => {
-  console.log(duties)
-  return <div>Duties</div>
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        const id = uuidv4()
+        // console.log(id)
+
+        return (
+          <div key={id} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 export default Duties
