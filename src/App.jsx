f63df0db135f152924edafcc84711c8b76714df7
Lawrence Caulker <locaulker@gmail.com>
2023-09-22 01:27:24 -0400

    implemented button container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import JobInfo from "./JobInfo"
+import BtnContainer from "./BtnContainer"
 
 const url = "https://course-api.com/react-tabs-project"
 
@@ -30,6 +31,7 @@ const App = () => {
   return (
     <section className="jobs-center">
       {/* <button className=""></button> */}
+      <BtnContainer jobs={jobs} />
       {/* job info */}
       <JobInfo jobs={jobs} />
     </section>
